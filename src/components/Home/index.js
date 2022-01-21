c0ce4e280ae7c2adc7ac31d3df4d8e8bf75e5469
Pham Thai Hung <hpham2687@gmail.com>
2022-01-21 15:53:53 +0700

    fix page number return string

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,7 @@ import PostList from "./Post/PostList";
 export default function Home() {
   const dispatch = useDispatch();
   const pageEndRef = useRef(null);
-  const { data, loading, error, page, maximumNumOfPages } = useSelector(
+  const { data, loading, error, page, maximumNumOfPages, search } = useSelector(
     (state) => state.posts
   );
   const { isLoggedIn } = useSelector((state) => state.auth);
@@ -22,8 +22,8 @@ export default function Home() {
   };
 
   useEffect(() => {
-    if (page != 1) pageEndRef?.current.scrollIntoView({ behavior: "smooth" });
-  }, [data, page]);
+    if (page !== 1) pageEndRef?.current.scrollIntoView({ behavior: "smooth" });
+  }, [page]);
 
   useEffect(() => {
     dispatch(getPostsAction({ page: 1, items_per_page: 10 }));
@@ -35,13 +35,10 @@ export default function Home() {
 
   const noPost2Show = (
     <NoPostToShowWrapper>
-      <h4>No posts to show.</h4>
+      <h4>No posts{search && ` with keyword "${search}" `}to show.</h4>
       {!isLoggedIn ? (
         <p>
-          <Link style={{ display: "inlines" }} to="/login">
-            Login
-          </Link>{" "}
-          to create posts
+          <Link to="/login">Login</Link> to create posts
         </p>
       ) : (
         <Link to="/add-post">Create post</Link>
